c9549b06894344be4a40849ad5c252e3c5c82b1e
Patrick Murani <patrickmurani@gmail.com>
2025-04-01 09:21:29 +0300

    Color configurations

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Benefits from "@/components/home/Benefits";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Home</h1>
+
+      {/* Section 4 - Benefits */}
+      <Benefits />
+    </main>
+  );
+}
