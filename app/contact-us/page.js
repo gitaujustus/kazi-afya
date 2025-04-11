a650633a8dc724518d119caf5c5990de85f45183
Justus <jgathiru02@gmail.com>
2025-04-11 14:31:09 +0300

    completed

--- app/contact-us/page.js
+++ app/contact-us/page.js
@@ -5,9 +5,9 @@ import ContactForm from "@/components/contausForm";
 
 const Contacts = () => {
   return (
-    <main className="flex flex-col md:flex-row">
+    <main className="flex flex-col md:flex-row border-b border-white">
       {/* locations */}
-      <div className="pt-[80px] pb-[65px] sm:pt-[100px] md:pt-[150px] xl:pt-[207px] px-[20px] sm:px-[30px] md:px-[42px] xl:px-[56px] gap-[10px] w-full md:min-w-[37vw] bg-wizard-white">
+      <div className="pt-[80px] pb-[65px] sm:pt-[100px] md:pt-[150px] xl:pt-[207px] px-[20px] sm:px-[32px] md:px-[48px] xl:px-[76px] gap-[10px] w-full md:min-w-[37vw] bg-wizard-white">
         <h1 className="text-[48px] sm:text-[60px] md:text-[72px] xl:text-[96px] leading-[120%] font-helvetica-bold text-london-rain">
           Our locations
         </h1>
